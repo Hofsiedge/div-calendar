8f0d1e3e464ef9d86f81b2d5307f41c172e923f1
Hofsiedge <zuev.ilia.al@yandex.ru>
2020-04-18 06:03:54 +0300

    Hotfix on Post serializer

--- post/serializers.py
+++ post/serializers.py
@@ -5,11 +5,12 @@ from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
 
-    date        = serializers.CharField(source='post.formatted_date', read_only=True)
-    ticker      = serializers.CharField(source='post.security.ticker', read_only=True)
+    date        = serializers.CharField(source='formatted_date', read_only=True)
+    ticker      = serializers.CharField(source='security.ticker', read_only=True)
+    logo        = serializers.CharField(source='security.logo', read_only=True)
 
     class Meta:
         model = Post
-        fields = ['date', 'ticker', 'title', 'text', 'source', 'poster', 'link']
+        fields = ['date', 'ticker', 'logo', 'title', 'text', 'source', 'poster', 'link']
         read_only_fields = ['date', 'ticker']
 
