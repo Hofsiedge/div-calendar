28114b7e1e6509b2325f5f80f2f992794ffc9703
Hofsiedge <zuev.ilia.al@yandex.ru>
2020-04-18 22:07:06 +0300

    Unique ticker

--- security/models.py
+++ security/models.py
@@ -5,7 +5,7 @@ class Security(models.Model):
     """ A stock or a bond"""
 
     # TODO: index by ticker
-    ticker      = models.CharField(max_length=25)
+    ticker      = models.CharField(max_length=25, unique=True)
     name        = models.CharField(max_length=150)
     logo        = models.URLField(max_length=200, blank=True)
     currency    = models.CharField(max_length=3, blank=True)
