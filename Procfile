fe6b2bfc7c16278db50e43c44f52c658c4d4c39d
Hofsiedge <zuev.ilia.al@yandex.ru>
2020-04-27 01:32:56 +0300

    Added transliteration testing

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-release: python manage.py migrate
+release: python manage.py migrate && python manage.py test
 web: daphne divcalendar.asgi:application  --port $PORT --bind 0.0.0.0 -v2
--- misc/tests.py
+++ misc/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .services import Transliterator
 
-# Create your tests here.
+class TransliteratorTestCase(TestCase):
+
+    def setUp(self):
+        self.transliterator = Transliterator('mappings')
+
+
+    def test_enru(self):
+        pairs = [
+            ('sberbank 123 @ 4', 'сбербанк 123  4'),
+            ('gazprom neft _ 2', 'газпром нефт  2'),
+            ('yandex-8', 'яндекс-8'),
+        ]
+
+        for pair in pairs:
+            self.assertEqual(self.transliterator.translit(pair[0], 'en', 'ru'), pair[1])
+
+
+    def test_ruen(self):
+        pairs = [
+            ('яндекс', 'yandex'),
+            ('м.видео', 'm.video'),
+            ('мэил ру', 'mail ru'),
+        ]
+
+        for pair in pairs:
+            self.assertEqual(self.transliterator.translit(pair[0], 'ru', 'en'), pair[1])
